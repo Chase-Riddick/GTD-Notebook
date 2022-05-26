ba01a53a982520b1f4f18e7d6653d01f3577c6e3
Chase Riddick <chaseriddick@outlook.com>
2022-05-26 13:21:34 -0700

    Login Route API Established

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,38 +1,13 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { User } = require('../../db/models');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-User'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+router.use('/session', sessionRouter);
 
-router.get(
-  '/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.use('/users', usersRouter);
 
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
