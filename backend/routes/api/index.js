71c4d7dd6c230983ebcbb94a44e6368906566800
Chase Riddick <chaseriddick@outlook.com>
2022-05-26 18:27:10 -0700

    attempt solve git issue

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,8 +6,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+//This is a test route.
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
