c5b778b74836034623e2f2871cec3499cc4a0276
Chase Riddick <chaseriddick@outlook.com>
2022-06-02 19:49:49 -0700

    Thursday evening CSS

---
+++ frontend/src/components/LogoutModal/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import Logout from '../Logout';
+import '../../css/Navigation.css'
+// import LoginForm from './LoginForm';
+
+export default function LogoutModal({user}) {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+        <i class="fa-solid fa-user" onClick={() => setShowModal(true)}></i>
+        {showModal && (
+            <Modal onClose={() => setShowModal(false)}>
+                <Logout user={user} setShowModal={setShowModal}/>
+            </Modal>
+        )}
+    </>
+  );
+}
